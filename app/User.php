dc5eab2c0691da79c737671b6f981d5e15704b7b
cbasil <cbasil@126.com>
2016-10-12 17:39:46 +0800

    注册api的实现1

--- app/User.php
+++ app/User.php
@@ -2,25 +2,37 @@
 
 namespace App;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Request;
+use Hash;
 
-class User extends Authenticatable
+class User extends Model
 {
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    public function signup()
+    {
+    	$username=Request::get('username');
+    	$password=Request::get('password');
+    	// 检查用户名和密码是否为空
+    	if(!($username && $password))
+    		return ['status'=>0,'msg'=>'用户名和密码不可为空'];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    	//检查用户名是否存在
+    	$user_exists=$this
+    	    ->where('username',$username)
+    	    ->exists();
+
+    	    if($user_exists)
+    	    	return ['status'=>0,'msg'=>'用户名已存在'];
+    	    	
+    	//加密密码
+    	$hashed_password = bcrypt($password);   
+      
+    	//存入数据库
+    	$this->password = $hashed_password;
+    	$this->username = $username;
+    	if($this->save())
+    		return ['status' => 1,'id' => $this -> id];
+		    else
+		    return ['status' => 0, 'msg' => 'db insert failed'];
+	}
 }
